a5f08011d01c7502c873980ed05adfd5e8dc28c0
Péter Takács <hhike@outlook.com>
2016-12-10 16:48:47 +0100

    Added TestHarness project.

---
+++ series1/TestHarness/src/TestClassB.java
@@ -0,0 +1,22 @@
+public class TestClassB {
+	
+	// Test ignore comments
+	// Test ignore empty lines
+	
+	private int propertyOne;
+	
+	private int propertyTwo;
+	private int propertyThree;
+	
+	public int breaker;
+	
+	// Test ignore indentation
+	public void TestMethod()
+	{
+		int variableA;
+		for (int i=0; i<3; i++)
+		{
+		System.out.println(i);
+		}
+	}
+}
---
+++ series1/TestHarness/src/TestClassC.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Test Ignore multiline.
+ * @author Peter
+ *
+ */
+public class TestClasC {
+	
+	// Test multifile
+	public void TestMethod()
+	{
+		for (int i=0; i<3; i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
